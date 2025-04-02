d2942d79217aaf4c719ff4a97b82d36eebda0200
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-04-02 19:52:21 -0300

    Adding css and js

---
+++ javascript/home.js
@@ -0,0 +1,25 @@
+// Store the selected elements that we are going to use. 
+const hamburger = document.querySelector("#myButton");
+const navLinks = document.querySelector("#navBar");
+
+// Add a click event listener to the hamburger button
+hamburger.addEventListener("click", () => {
+    navBar.classList.toggle("open");
+    hamburger.textContent = navBar.classList.contains("open") ? "✖" : "☰";
+});
+
+// Function for the current year in the footer
+function getCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("current_year").textContent = currentYear;
+}
+
+// Function for the last modified date
+function getLastModified() {
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+}
+
+// Calling functions
+getCurrentYear();
+getLastModified();

8558466a640d7a337dc57f8ef1dbda2659a5b08d
Mayza Barbosa <mayzabar26@gmail.com>
2025-06-25 12:36:27 -0300

    Corrigindo links e menu hamburguer

--- javascript/home.js
+++ javascript/home.js
@@ -1,20 +1,20 @@
-// Store the selected elements that we are going to use. 
+//Store the selected elements that we are going to use. 
 const hamburger = document.querySelector("#myButton");
 const navLinks = document.querySelector("#navBar");
 
-// Add a click event listener to the hamburger button
+//Add a click event listener to the hamburger button
 hamburger.addEventListener("click", () => {
-    navBar.classList.toggle("open");
-    hamburger.textContent = navBar.classList.contains("open") ? "✖" : "☰";
+    navLinks.classList.toggle("open");
+    hamburger.textContent = navLinks.classList.contains("open") ? "✖" : "☰";
 });
 
-// Function for the current year in the footer
+//Function for the current year in the footer
 function getCurrentYear() {
     const currentYear = new Date().getFullYear();
     document.getElementById("current_year").textContent = currentYear;
 }
 
-// Function for the last modified date
+//Function for the last modified date
 function getLastModified() {
     const lastModified = document.lastModified;
     document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
